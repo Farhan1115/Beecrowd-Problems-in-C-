e98a35db503e689faf436d83a18207052977b238
Farhan1115 <109974258+Farhan1115@users.noreply.github.com>
2022-08-04 11:52:53 +0600

    Add files via upload

---
+++ 1002beecrowd.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double R, pi=3.14159, area;
+    cin>>R;
+    area = pi*R*R;
+    cout<<fixed<<setprecision(4);
+    cout<<"A="<<area<<endl;
+    return 0;
+}
