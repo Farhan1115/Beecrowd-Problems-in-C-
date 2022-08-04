1efd405746af902e53344e3daecd6baa2def7823
Farhan1115 <109974258+Farhan1115@users.noreply.github.com>
2022-08-04 15:21:35 +0600

    Add files via upload

---
+++ 1014beecrowd.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int X;
+    double Y, result;
+    cin>>X>>Y;
+    result = X/Y;
+    cout<<fixed<<setprecision(3);
+    cout<<result<<" km/l"<<endl;
+    return 0;
+}
