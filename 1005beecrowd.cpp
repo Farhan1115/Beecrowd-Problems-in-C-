aedd686f2c816f66290524bfd88c6321e3c33961
Farhan1115 <109974258+Farhan1115@users.noreply.github.com>
2022-08-04 11:29:13 +0600

    Add files via upload

---
+++ 1005beecrowd.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a, b, average;
+    cin>>a>>b;
+    average = ( (a*3.5)+(b*7.5) )/11.0;
+    cout<<showpoint<<fixed<<setprecision(5);
+    cout<<"MEDIA = "<<average<<endl;
+}
