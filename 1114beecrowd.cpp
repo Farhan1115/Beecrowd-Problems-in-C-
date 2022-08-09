6302ac570efc8d4b0135de5676a9d2cb31838e2f
Farhan1115 <109974258+Farhan1115@users.noreply.github.com>
2022-08-09 19:59:29 +0600

    Add files via upload

---
+++ 1114beecrowd.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int N;
+    while(1)
+    {
+        cin>>N;
+        if( N==2002 )
+        {
+            cout<<"Acesso Permitido"<<endl;
+            break;
+        }
+        else
+        {
+            cout<<"Senha Invalida"<<endl;
+        }
+    }
+    return 0;
+}
